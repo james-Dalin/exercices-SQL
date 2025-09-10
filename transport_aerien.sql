e29e3f5064d9f3685f92e55570f11af1845c4979
drajoan258 <charlestedison@gmail.com>
2025-09-10 18:07:16 +0200

    Fin des exercices sur SQL du module 5

--- transport_aerien.sql
+++ transport_aerien.sql
@@ -8,51 +8,64 @@ USE transport_aerien;
 -- GROUP BY auteur_id
 -- HAVING COUNT(*) > 1;
 
-/*Exercice2 */
--- Petit 1
+/*Exercice 2*/
+-- Petit 1 (Sans aide) Afficher toutes les colonnes de la table avions
 SELECT * 
 FROM avions;
--- Petit 2
+-- Petit 2 (Sans aide) Afficher les modèles et constructeurs des avions
 SELECT modele, constructeur 
 FROM avions;
--- Petit 3
+-- Petit 3 (Sans aide) Afficher les capacités des avions qui sont supérieures à 300
 SELECT * 
 FROM avions 
 WHERE capacite > 300;
--- Petit 4
+-- Petit 4 (Sans aide) Afficher les avions dont l'autonomie est inférieure à 7000
 SELECT *
 FROM avions
 WHERE autonomie_km < 7000;
--- Petit 5
+-- Petit 5 (Sans aide) Afficher les avions qui ne sont pas en services
 SELECT *
 FROM avions
 WHERE en_service = FALSE;
--- Petit 6
+-- Petit 6 (Sans aide) Sélectionner les avions dont leurs constructeurs est Boeing
 SELECT *
 FROM avions
 WHERE constructeur = 'Boeing';
--- Petit 7
-
--- Petit 8
-
--- Petit 9
+-- Petit 7 (Aide de l'ia -_-") Afficher la capacité des avions entre 100 et 300
+SELECT *
+FROM avions
+WHERE capacite BETWEEN 100 AND 300;
+-- Petit 8 (Sans aide) Afficher tous les constructeurs des avions
+SELECT DISTINCT constructeur
+FROM avions;
+-- Petit 9 (Sans aide) Montrer l'autonomie des avions ans l'ordre coirssant
 SELECT *
 FROM avions
 ORDER BY autonomie_km ASC ;
--- Petti 10
+-- Petti 10 (Sans aide) Afficher les capacités des avions dans l'ordre décroissant
 SELECT *
 FROM avions
 ORDER BY capacite DESC;
--- Petit 11
-
--- Petit 12
-
--- Petit 13
+-- Petit 11 Selection des avions ayant les plus grandes autonomies
+SELECT *
+FROM avions
+ORDER BY autonomie_km DESC
+LIMIT 3;
+-- Petit 12 Afficher les 5 avions avec les plus faibles capacités
+SELECT *
+FROM avions
+ORDER BY capacite ASC
+LIMIT 5;
+-- Petit 13 Afficher les avions dont l'autonomie est comprise entre 6000 et 13000
+SELECT *
+FROM avions
+WHERE autonomie_km >= 6000 AND autonomie_km <= 13000;
 /*Exercice 2*/
 
+DROP TABLE avions;
  -- Table des Avions
 CREATE TABLE IF NOT EXISTS avions (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     modele VARCHAR(100) NOT NULL,
     constructeur VARCHAR(100) NOT NULL,
     capacite INT NOT NULL,
